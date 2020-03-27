ba5467bb54314faf2c0497d1eae2ff292a160f77
developernka <developer.nka@gmail.com>
2020-03-27 21:53:12 +0530

    update

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,46 @@
+$('#main').hide();
+$('#loading').show();
 $.get(
   'https://spreadsheets.google.com/feeds/list/1nzXUdaIWC84QipdVGUKTiCSc5xntBbpMpzLm6Si33zk/od6/public/values?alt=json',
   function(json) {
     console.log(json.feed.entry.length);
+    var tbodyHtm = '';
+    $(json.feed.entry).each(function(r, row) {
+      if (row['gsx$dateannounced'])
+        var trHtm = `<tr id="${row['gsx$patientnumber']}">`;
+      trHtm += `<td>${row['gsx$dateannounced']}</td>`;
+      trHtm += `<td>${row['gsx$currentstatus'](
+        row['gsx$dateannounced'] != row['gsx$statuschangedate']
+          ? ' on ' + row['gsx$statuschangedate']
+          : ''
+      )}</td>`;
+      trHtm += `<td>${row['gsx$detectedstate']}</td>`;
+      trHtm += `<td>${
+        row['gsx$detecteddistrict'] == row['gsx$detectedcity']
+          ? row['gsx$detecteddistrict']
+          : (
+              row['gsx$detecteddistrict'] +
+              ' ' +
+              row['gsx$detecteddistrict']
+            ).trim()
+      }</td>`;
+      trHtm += `<td>${(
+        row['gsx$gender'] +
+        ' ' +
+        row['gsx$agebracket']
+      ).trim()}</td>`;
+      trHtm += `<td>${row['gsx$note']}</td>`;
+      trHtm += `<td>${
+        row['gsx$nationality'].toLowerCase().indexOf('india') == -1
+          ? row['gsx$nationality']
+          : ''
+      }</td>`;
+      trHtm += `<td><a href="${row['gsx$source1']}">View</a> <a href="${row['gsx$source2']}">View</a> <a href="${row['gsx$source3']}">View</a></td>`;
+      trHtm += `</tr>`;
+      tbodyHtm += trHtm;
+    });
+    $('#main>tbody').html(tbodyHtm);
+    $('#loading').hide();
+    $('#main').show();
   }
 );
