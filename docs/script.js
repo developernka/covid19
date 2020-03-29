5dde8aba03ef9f651e913c7bd2e8cef61988b814
developernka <developer.nka@gmail.com>
2020-03-29 22:59:29 +0530

    update

--- docs/script.js
+++ docs/script.js
@@ -6,37 +6,39 @@ $.get(
     console.log(json.feed.entry.length);
     var tbodyHtm = '';
     $(json.feed.entry).each(function(r, row) {
-      if (row['gsx$currentstatus']['$t'].trim() != '')
+      if (row['gsx$currentstatus']['$t'].trim() != '') {
         var trHtm = `<tr id="${row['gsx$patientnumber']['$t']}">`;
-      trHtm += `<td>${row['gsx$dateannounced']['$t']}</td>`;
-      trHtm += `<td>${row['gsx$currentstatus']['$t'] +
-        (row['gsx$dateannounced']['$t'] != row['gsx$statuschangedate']['$t']
-          ? ' on ' + row['gsx$statuschangedate']['$t']
-          : '')}</td>`;
-      trHtm += `<td>${row['gsx$detectedstate']['$t']}</td>`;
-      trHtm += `<td>${
-        row['gsx$detecteddistrict']['$t'] == row['gsx$detectedcity']['$t']
-          ? row['gsx$detecteddistrict']['$t']
-          : (
-              row['gsx$detecteddistrict']['$t'] +
-              ' ' +
-              row['gsx$detecteddistrict']['$t']
-            ).trim()
-      }</td>`;
-      trHtm += `<td>${(
-        row['gsx$gender']['$t'] +
-        ' ' +
-        row['gsx$agebracket']['$t']
-      ).trim()}</td>`;
-      trHtm += `<td>${row['gsx$notes']['$t']}</td>`;
-      trHtm += `<td>${
-        (row['gsx$nationality']['$t'] + '').toLowerCase().indexOf('india') == -1
-          ? row['gsx$nationality']['$t']
-          : ''
-      }</td>`;
-      trHtm += `<td><a href="${row['gsx$source1']['$t']}">View</a> <a href="${row['gsx$source2']['$t']}">View</a> <a href="${row['gsx$source3']['$t']}">View</a></td>`;
-      trHtm += `</tr>`;
-      tbodyHtm = trHtm + tbodyHtm;
+        trHtm += `<td>${row['gsx$dateannounced']['$t']}</td>`;
+        trHtm += `<td>${row['gsx$currentstatus']['$t'] +
+          (row['gsx$dateannounced']['$t'] != row['gsx$statuschangedate']['$t']
+            ? ' on ' + row['gsx$statuschangedate']['$t']
+            : '')}</td>`;
+        trHtm += `<td>${row['gsx$detectedstate']['$t']}</td>`;
+        trHtm += `<td>${
+          row['gsx$detecteddistrict']['$t'] == row['gsx$detectedcity']['$t']
+            ? row['gsx$detecteddistrict']['$t']
+            : (
+                row['gsx$detecteddistrict']['$t'] +
+                ' ' +
+                row['gsx$detecteddistrict']['$t']
+              ).trim()
+        }</td>`;
+        trHtm += `<td>${(
+          row['gsx$gender']['$t'] +
+          ' ' +
+          row['gsx$agebracket']['$t']
+        ).trim()}</td>`;
+        trHtm += `<td>${row['gsx$notes']['$t']}</td>`;
+        trHtm += `<td>${
+          (row['gsx$nationality']['$t'] + '').toLowerCase().indexOf('india') ==
+          -1
+            ? row['gsx$nationality']['$t']
+            : ''
+        }</td>`;
+        trHtm += `<td><a href="${row['gsx$source1']['$t']}">View</a> <a href="${row['gsx$source2']['$t']}">View</a> <a href="${row['gsx$source3']['$t']}">View</a></td>`;
+        trHtm += `</tr>`;
+        tbodyHtm = trHtm + tbodyHtm;
+      }
     });
     $('#main>tbody').html(tbodyHtm);
     $('#loading').hide();
