0a7ba32b9a3abe15bba55e87ee4ce2ddea6c3d47
developernka <developer.nka@gmail.com>
2020-05-25 09:30:16 +0530

    update

--- docs/script.js
+++ docs/script.js
@@ -1,47 +1,80 @@
-$('#main').hide();
-$('#loading').show();
-$.get(
-  'https://spreadsheets.google.com/feeds/list/1nzXUdaIWC84QipdVGUKTiCSc5xntBbpMpzLm6Si33zk/od6/public/values?alt=json',
-  function(json) {
-    console.log(json.feed.entry.length);
-    var tbodyHtm = '';
-    $(json.feed.entry).each(function(r, row) {
-      if (row['gsx$currentstatus']['$t'].trim() != '') {
-        var trHtm = `<tr id="${row['gsx$patientnumber']['$t']}">`;
-        trHtm += `<td>${row['gsx$dateannounced']['$t']}</td>`;
-        trHtm += `<td>${row['gsx$currentstatus']['$t'] +
-          (row['gsx$dateannounced']['$t'] != row['gsx$statuschangedate']['$t']
-            ? ' on ' + row['gsx$statuschangedate']['$t']
-            : '')}</td>`;
-        trHtm += `<td>${row['gsx$detectedstate']['$t']}</td>`;
-        trHtm += `<td>${
-          row['gsx$detecteddistrict']['$t'] == row['gsx$detectedcity']['$t']
-            ? row['gsx$detecteddistrict']['$t']
-            : (
-                row['gsx$detecteddistrict']['$t'] +
-                ' ' +
-                row['gsx$detecteddistrict']['$t']
-              ).trim()
-        }</td>`;
-        trHtm += `<td>${(
-          row['gsx$gender']['$t'] +
-          ' ' +
-          row['gsx$agebracket']['$t']
-        ).trim()}</td>`;
-        trHtm += `<td>${row['gsx$notes']['$t']}</td>`;
-        trHtm += `<td>${
-          (row['gsx$nationality']['$t'] + '').toLowerCase().indexOf('india') ==
-          -1
-            ? row['gsx$nationality']['$t']
-            : ''
-        }</td>`;
-        trHtm += `<td><a href="${row['gsx$source1']['$t']}">View</a> <a href="${row['gsx$source2']['$t']}">View</a> <a href="${row['gsx$source3']['$t']}">View</a></td>`;
-        trHtm += `</tr>`;
-        tbodyHtm = trHtm + tbodyHtm;
-      }
+var db = new Dexie("covid19_database");
+db.version(1).stores({
+  patient: 'entryid,patientnumber,statecode,detectedstate,detecteddistrict,[statecode+detecteddistrict+dateannounced]',
+  state: 'statecode'
+});
+
+function getLatest(n) {
+  $.get('https://api.covid19india.org/raw_data' + n + '.json', function (resp) {
+    resp.raw_data.map(row => {
+      row.dateannounced = row.dateannounced.split('/').reverse().join("-");
+      return row;
     });
-    $('#main>tbody').html(tbodyHtm);
-    $('#loading').hide();
-    $('#main').show();
-  }
-);
+    db.patient.bulkPut(resp.raw_data, console.log);
+    try {
+      let lastDate = resp.raw_data[resp.raw_data.length - 1].dateannounced.split('/').reverse().join('/');
+      if (new Date(lastDate).getTime() < new Date().getTime() - 2 * 24 * 3600 * 1000) {
+        localStorage.latestDb = n + 1;
+        getLatest(n + 1);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
+let latestDb = 1 | localStorage.latestDb;
+getLatest(latestDb);
+
+function getStateDistrict() {
+  $.get('https://api.covid19india.org/v2/state_district_wise.json', function (resp) {
+    db.state.bulkPut(resp, console.log);
+  });
+}
+getStateDistrict();
+
+showSearchBar();
+function showSearchBar() {
+  db.state.toArray().then(states => {
+    let htm = "<option value=''>Select State</option>";
+    states.forEach(state => {
+      htm += `<option value="${state.statecode}">${state.state}</option>`;
+    });
+    $("#state").html(htm);
+    $("#state").change(function () {
+      db.state.get($(this).val()).then(state => {
+        let htm = "<option value=''>Select District</option>";
+        state.districtData.forEach(district => {
+          htm += `<option value="${district.district}">${district.district}</option>`;
+        });
+        $("#district").html(htm);
+        $("#district").change(function () {
+          showTable({
+            statecode: $("#state").val(),
+            detecteddistrict: $("#district").val()
+          });
+        });
+      });
+    });
+    $("#dateannounced").change(function () {
+      showTable({
+        statecode: $("#state").val(),
+        detecteddistrict: $("#district").val(),
+        dateannounced: $("#dateannounced").val()
+      });
+    })
+  });
+}
+
+function showTable(query) {
+  console.time('showTable');
+  db.patient.where(query).reverse().sortBy('dateannounced').then(rows => {
+    console.log(rows);
+    let htm = "";
+    rows.forEach(row => {
+      htm += `<tr title='${JSON.stringify(row, null, 4)}' ><td>${row.dateannounced}</td><td>${row.detectedcity}</td><td>${row.patientnumber}</td><td>${row.gender}</td><td>${row.agebracket}</td><td>${row.numcases}</td></tr>`;
+      console.log('row');
+    });
+    $("#main tbody").html(htm);
+    console.timeEnd('showTable');
+  });
+}
